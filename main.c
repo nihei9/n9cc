7ac22b27a4a7c2d3385ed029e1bf2d3b082aa9d6
Ryo Nihei <nihei.dev@gmail.com>
2020-06-28 02:22:33 +0900

    Add *, / and ()

--- main.c
+++ main.c
@@ -24,6 +24,23 @@ struct Token {
 // a token now focused on
 Token *token;
 
+// the entier input source code
+char *user_input;
+
+// error reports an error and exits with exit code 1.
+void error_at(char *loc, char *fmt, ...) {
+	va_list ap;
+	va_start(ap, fmt);
+
+	int pos = loc - user_input;
+	fprintf(stderr, "%s\n", user_input);
+	fprintf(stderr, "%*s", pos, "");
+	fprintf(stderr, "^ ");
+	vfprintf(stderr, fmt, ap);
+	fprintf(stderr, "\n");
+	exit(1);
+}
+
 // error reports an error and exits with exit code 1.
 void error(char *fmt, ...) {
 	va_list ap;
@@ -47,7 +64,7 @@ bool consume(char op) {
 // If the check passed, it consumes the token. Otherwise, it reports an error and exits.
 void expect(char op) {
 	if (token->kind != TK_RESERVED || token->str[0] != op) {
-		error("expected: '%c'", op);
+		error_at(token->str, "expected '%c'", op);
 	}
 	token = token->next;
 }
@@ -57,7 +74,7 @@ void expect(char op) {
 // Otherwise, it reports an error and exits.
 int expect_number() {
 	if (token->kind != TK_NUM) {
-		error("expected: a number, actual: not a number");
+		error_at(token->str, "expected a number");
 	}
 	int val = token->val;
 	token = token->next;
@@ -78,11 +95,12 @@ Token *new_token(TokenKind kind, Token *cur, char *str) {
 	return tok;
 }
 
-// tokenize tokenizes a string passed.
-Token *tokenize(char *p) {
+// tokenize tokenizes `user_input`.
+Token *tokenize() {
 	Token head;
 	head.next = NULL;
 	Token *cur = &head;
+	char *p = user_input;
 
 	while (*p) {
 		if (isspace(*p)) {
@@ -90,7 +108,7 @@ Token *tokenize(char *p) {
 			continue;
 		}
 
-		if (*p == '+' || *p == '-') {
+		if (strchr("+-*/()", *p)) {
 			cur = new_token(TK_RESERVED, cur, p++);
 			continue;
 		}
@@ -101,11 +119,139 @@ Token *tokenize(char *p) {
 			continue;
 		}
 
-		error("failed to tokenize");
+		error_at(p, "invalid token");
 	}
 
 	new_token(TK_EOF, cur, p);
 	return head.next;
+}
+
+void print_tokens() {
+	printf("tokens:\n");
+	for (Token *tok = token; tok != NULL; tok = tok->next) {
+		switch (tok->kind) {
+		case TK_RESERVED:
+			printf("  TK_RESERVED\n");
+			break;
+		case TK_NUM:
+			printf("  TK_NUM %d\n", tok->val);
+			break;
+		case TK_EOF:
+			printf("  TK_EOF\n");
+			break;
+		}
+	}
+}
+
+typedef enum {
+			  ND_ADD,
+			  ND_SUB,
+			  ND_MUL,
+			  ND_DIV,
+			  ND_NUM,
+} NodeKind;
+
+typedef struct Node Node;
+
+// Node represents a node of an AST.
+struct Node {
+	NodeKind kind;
+	Node *lhs;
+	Node *rhs;
+	int val;
+};
+
+// new_node returns a new node.
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = kind;
+	node->lhs = lhs;
+	node->rhs = rhs;
+	return node;
+}
+
+// new_node returns a new node that represents an integer.
+Node *new_node_num(int val) {
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = ND_NUM;
+	node->val = val;
+	return node;
+}
+
+Node *mul();
+Node *primary();
+
+// expr = mul ("+" mul | "-" mul)*
+Node *expr() {
+	Node *node = mul();
+
+	for (;;) {
+		if (consume('+')) {
+			node = new_node(ND_ADD, node, mul());
+		} else if (consume('-')) {
+			node = new_node(ND_SUB, node, mul());
+		} else {
+			return node;
+		}
+	}
+}
+
+// mul = primary ("*" primary | "/" primary)*
+Node *mul() {
+	Node *node = primary();
+
+	for (;;) {
+		if (consume('*')) {
+			node = new_node(ND_MUL, node, primary());
+		} else if (consume('/')) {
+			node = new_node(ND_DIV, node, primary());
+		} else {
+			return node;
+		}
+	}
+}
+
+// primary = "(" expr ")" | num
+Node *primary() {
+	if (consume('(')) {
+		Node *node = expr();
+		expect(')');
+		return node;
+	}
+
+	return new_node_num(expect_number());
+}
+
+// gen generates asembly.
+void gen(Node *node) {
+	if (node->kind == ND_NUM) {
+		printf("  push %d\n", node->val);
+		return;
+	}
+
+	gen(node->lhs);
+	gen(node->rhs);
+
+	printf("  pop rdi\n");
+	printf("  pop rax\n");
+
+	switch (node->kind) {
+	case ND_ADD:
+		printf("  add rax, rdi\n");
+		break;
+	case ND_SUB:
+		printf("  sub rax, rdi\n");
+		break;
+	case ND_MUL:
+		printf("  imul rax, rdi\n");
+		break;
+	case ND_DIV:
+		printf("  cqo\n");
+		printf("  idiv rax, rdi\n");
+		break;
+	}
+
+	printf("  push rax\n");
 }
 
 int main(int argc, char **argv) {
@@ -114,24 +260,18 @@ int main(int argc, char **argv) {
 		return 1;
 	}
 
-	token = tokenize(argv[1]);
+	user_input = argv[1];
+	token = tokenize();
+	//	print_tokens();
+	Node *node = expr();
 
 	printf(".intel_syntax noprefix\n");
 	printf(".global main\n");
 	printf("main:\n");
-	
-	printf("  mov rax, %d\n", expect_number());
 
-	while (!at_eof()) {
-		if (consume('+')) {
-			printf("  add rax, %d\n", expect_number());
-			continue;
-		}
-		
-		expect('-');
-		printf("  sub rax, %d\n", expect_number());
-	}
-	
+	gen(node);
+
+	printf("  pop rax\n");
 	printf("  ret\n");
 	return 0;
 }
